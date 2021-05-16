59ca59487034ce96d72cda75e2981fae7b824df6
Way <way11229@gmail.com>
2021-05-16 21:59:31 +0800

    Way update.

--- line/main.go
+++ line/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(res, "Hello World!")
+		fmt.Fprint(res, "Hello World line!")
 	})
 
 	http.ListenAndServe(":80", nil)
