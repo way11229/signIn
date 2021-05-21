8145a1674367974ce76cf5f908def95f03df8974
Way <way11229@gmail.com>
2021-05-21 22:32:57 +0800

    Way update.

--- gateway/main.go
+++ gateway/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
-
-	httpHandler "signIn/gateway/delivery/http"
+	// httpHandler "signIn/gateway/delivery/http"
 )
 
 func main() {
 	log.Info("Sign in gateway server start")
 
 	r := gin.Default()
-	httpHandler.NewHttpHandler(r)
+	// httpHandler.NewHttpHandler(r)
+	r.LoadHTMLGlob("view/*")
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+		})
+	})
 
 	log.Fatal(r.Run(":80"))
 }
