f806f9c7601c4d249ed671f0a0e6f4983359cafc
Way <way11229@gmail.com>
2021-06-02 21:31:41 +0800

    Way update.

--- gateway/delivery/http/httpHandler.go
+++ gateway/delivery/http/httpHandler.go
@@ -24,8 +24,6 @@ func (h *HttpHandler) LineSignIn(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{})
 	}
 
-	c.Header("Access-Control-Allow-Origin", "signin-frontend.selldarity.com")
-	c.Header("Access-Control-Allow-Methods", "POST, GET")
 	c.JSON(http.StatusOK, gin.H{
 		"code": verifyCode,
 	})
