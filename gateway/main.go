e65c1ef9f924dc878b02d19681ad3cddae1c899e
Way <way11229@gmail.com>
2021-06-02 21:22:45 +0800

    Way add gateway middleware.

--- gateway/main.go
+++ gateway/main.go
@@ -5,12 +5,15 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	httpHandler "signIn/gateway/delivery/http"
+
+	cors "signIn/gateway/middleware/cors"
 )
 
 func main() {
 	log.Info("Sign in gateway server start")
 
 	r := gin.Default()
+	r.Use(cors.CORSMiddleWare())
 	httpHandler.NewHttpHandler(r)
 
 	log.Fatal(r.Run(":80"))
